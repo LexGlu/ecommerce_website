44fcdb7fc0e3b311e9763e13ebf07683c2f843db
Oleksandr Glushchenko <117204585+LexGlu@users.noreply.github.com>
2023-04-10 17:12:43 +0300

    dockerize app

--- ecom_website/settings.py
+++ ecom_website/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-6q=oyxh2!pdh69v!!u+p@86(4z^#1j!+$sm&=#5uo_lnj4*(5@"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['localhost', ]
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
@@ -89,14 +91,22 @@ WSGI_APPLICATION = "ecom_website.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'ecom_db',
+    #     'USER': 'ecom-admin',
+    #     'PASSWORD': 'ecom_password',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432'
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ecom_db',
-        'USER': 'ecom-admin',
-        'PASSWORD': 'ecom_password',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
+            'ENGINE': os.environ.get('SQL_ENGINE', 'django.db.backends.db.sqlite3'),
+            'NAME': os.environ.get('SQL_DATABASE', BASE_DIR / 'db.sqlite3'),
+            'USER': os.environ.get('SQL_USER', 'user'),
+            'PASSWORD': os.environ.get('SQL_PASSWORD', 'password'),
+            'HOST': os.environ.get('SQL_HOST', 'localhost'),
+            'PORT': os.environ.get('SQL_PORT', '5432')
+        }
 }
 
 
